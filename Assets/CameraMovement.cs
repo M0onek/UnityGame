0c60b2d8461c5ebf41dbf0f19efa288bcd974e99
szymon-k <szymon.krzywiecki@codetain.co>
2019-12-04 20:25:22 +0100

    Initial commit

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Rect rect;
+    public Transform player;
+
+    void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+        if (player.position.x > rect.x && player.position.x < rect.x + rect.width) pos.x = player.position.x;
+        if (player.position.y > rect.y && player.position.y < rect.y + rect.height) pos.y = player.position.y;
+        transform.position = pos;
+    }
+}
