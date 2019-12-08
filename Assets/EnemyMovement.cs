9abb87b23c425c6c3479a0684344fb2bee64d378
szymon-k <szymon.krzywiecki@codetain.co>
2019-12-08 17:47:15 +0100

    enemy animation and A*

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Animator animator;
+    public AIPath aiPath;
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Horizontal", aiPath.desiredVelocity.x);
+        animator.SetFloat("Vertical", aiPath.desiredVelocity.y);
+        animator.SetFloat("Speed", aiPath.desiredVelocity.sqrMagnitude);
+    }
+}
